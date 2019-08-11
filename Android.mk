2927554c8152b8c67a86b07a326832d6cc22e815
nebrassy <nebras30@gmail.com>
2019-08-11 20:36:24 +0300

    Hello V4A

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := ViPER4AndroidFX
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SHARED_LIBRARIES := libv4a_fx
+LOCAL_SRC_FILES := ViPER4AndroidFX.apk
+include $(BUILD_PREBUILT)
+
+
+    ifneq (,$(filter $(TARGET_ARCH), x86 x86_64))
+       V4A_TARGET := x86/libv4a_fx.so
+    else
+        V4A_TARGET := armeabi-v7a/libv4a_fx.so
+    endif
+include $(CLEAR_VARS)
+LOCAL_MODULE := libv4a_fx
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/soundfx
+LOCAL_SRC_FILES := $(V4A_TARGET)
+include $(BUILD_PREBUILT)
---
+++ README.md
@@ -0,0 +1,7 @@
+Prebuilt ViPER4AndroidFX
+
+add this to your audio_effects.xml
+
+            <library name="v4a_fx" path="libv4a_fx.so"/>
+
+            <effect name="v4a_standard_fx" library="v4a_fx" uuid="41d3c987-e6cf-11e3-a88a-11aba5d5c51b"/>
